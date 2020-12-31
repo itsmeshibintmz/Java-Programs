f44344e6f63d01d290ef0319ad5b66c9f60b6f70
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2020-12-31 16:09:46 +0530

    Add files via upload
    1.	Write a program to accept a number and then to find the reverse of the number and sum of its digits.

---
+++ RevSumTest.java
@@ -0,0 +1,33 @@
+public class RevSumTest {
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        RevSum rev = new RevSum(number);
+        rev.calculateReverse();
+        rev.calculateSumOfDigits();
+    }
+}
+
+class RevSum {
+    int reverse =0 ,numberToFind,sumOfDigit,numberToFindSum;
+    RevSum(int numberToRev) {
+        numberToFind = numberToRev;
+        numberToFindSum = numberToRev;
+    }
+    void calculateReverse() {
+        while(numberToFind!=0) {
+            int digit = numberToFind % 10;
+            reverse = reverse * 10 + digit;
+            numberToFind /= 10;
+        }
+        System.out.println("Reversed Number: " + reverse);
+    }
+    void calculateSumOfDigits() {
+        while(numberToFindSum > 0)
+        {
+            int numbers = numberToFindSum % 10;
+            sumOfDigit = sumOfDigit + numbers;
+            numberToFindSum = numberToFindSum / 10;
+        }
+        System.out.println("Sum of Digits:"+sumOfDigit);
+    }
+}
