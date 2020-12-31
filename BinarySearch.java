194d3cf9d62d8f5e3b43a933bced109b9ddbed28
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2020-12-31 16:19:38 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,4 @@
+//Java program to implement binary search.
 class BinarySearch{  
     public static void binarySearch(int arr[], int first, int last, int key){  
       int mid = (first + last)/2;  
